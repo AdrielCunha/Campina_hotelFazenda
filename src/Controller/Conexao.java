f66a35b42088e666177c409e380692ef9071138f
GiovannaMoraes <gm04112002@gmail.com>
2020-06-19 13:53:03 -0300

    controller.conexao

--- src/Controller/Conexao.java
+++ src/Controller/Conexao.java
@@ -5,12 +5,43 @@
  */
 package Controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author gm041
  */
 public class Conexao {
 
-   
+   private static final String host = "localhost";
+    private static String database = "bd_fisico1";
+    private static String usuario = "root";
+    private static String senha = "";
+    private static String url = "jdbc:mysql://" + host + "/" + database;
+    
+    private Connection conn;
+     
+    public Conexao() throws SQLException, ClassNotFoundException
+    {        
+        Class.forName("com.mysql.jdbc.Driver");
+
+        conn = (Connection) DriverManager.getConnection(url, usuario, senha);        
+    }
+    
+    
+    
+    public Connection getConn() {
+        return this.conn;
+    }
+     
+    
+    
+    public void fecharConexao() throws SQLException
+    {        
+       conn.close();
+    }
+    
     
 }
